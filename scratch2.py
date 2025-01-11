6b87794a0bce1c93731ad5cd94b8f828647eeda8
MrBubb1es <lance04preston@gmail.com>
2025-01-11 11:53:43 -0600

    cleaning up files

--- scratch2.py
+++
@@ -1,5 +0,0 @@
-a == """if (self.idx_size() == RegSize::Byte) && !self.page_crossed {
-                    extra_clocks = Cpu65c816::ONE_CYCLE;
-                } else {
-                    extra_clocks = Cpu65c816::TWO_CYCLE;
-                }"""
